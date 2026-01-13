d81d6c5e558568d4bceefd19abb046e2705fa964
Olim Yuldoshev <oyuldoshev39@gmail.com>
2026-01-13 15:30:35 +0500

    Added

--- screens/application/Home.tsx
+++ screens/application/Home.tsx
@@ -1,6 +1,15 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 const Home = () => {
   return (
@@ -17,8 +26,51 @@ const Home = () => {
             </View>
             <MaterialIcons name="notifications-none" size={41} color="black" />
           </View>
-          <View style={styles.headerBlock2}></View>
-          <View style={styles.headerBlock3}></View>
+          <View style={styles.headerBlock2}>
+            <Ionicons
+              name="search"
+              size={30}
+              color="black"
+              style={styles.searchIconOpenModal}
+            />
+
+            <TextInput
+              style={styles.inputOpenModalSearch}
+              placeholder="Search Here"
+            />
+          </View>
+          <ScrollView
+            contentContainerStyle={styles.headerBlock3ScrollView}
+            style={styles.headerBlock3}
+            
+          >
+            <Pressable style={[styles.filterBtn, styles.filterBtnActive]}>
+              <Text style={[styles.filterBtnText, styles.filterBtnTextActive]}>
+                All
+              </Text>
+            </Pressable>
+            <Pressable style={[styles.filterBtn, styles.filterBtnInactive]}>
+              <Text
+                style={[styles.filterBtnText, styles.filterBtnTextInactive]}
+              >
+                Best book
+              </Text>
+            </Pressable>
+            <Pressable style={[styles.filterBtn, styles.filterBtnInactive]}>
+              <Text
+                style={[styles.filterBtnText, styles.filterBtnTextInactive]}
+              >
+                Classics
+              </Text>
+            </Pressable>
+            <Pressable style={[styles.filterBtn, styles.filterBtnInactive]}>
+              <Text
+                style={[styles.filterBtnText, styles.filterBtnTextInactive]}
+              >
+                Fantasy
+              </Text>
+            </Pressable>
+          </ScrollView>
         </View>
         <View style={styles.sectionHomeComponent}></View>
       </View>
@@ -38,7 +90,6 @@ const styles = StyleSheet.create({
     paddingTop: 45,
     paddingHorizontal: 10,
   },
-  sectionHomeComponent: {},
   headerBlock1: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -55,6 +106,40 @@ const styles = StyleSheet.create({
     fontSize: 26,
     fontWeight: "400",
   },
-  headerBlock2: {},
-  headerBlock3: {},
+  headerBlock2: {
+    marginTop: 10,
+    position: "relative",
+  },
+  searchIconOpenModal: {
+    position: "absolute",
+    zIndex: 5,
+    top: 9.5,
+    left: 9.5,
+  },
+  inputOpenModalSearch: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 4,
+    backgroundColor: "#fff",
+    fontSize: 20,
+    fontWeight: "600",
+    borderRadius: 24,
+    paddingLeft: 55,
+  },
+  headerBlock3ScrollView: {},
+  headerBlock3: {
+  
+  },
+  filterBtn: {},
+  filterBtnText: {},
+  filterBtnActive: {},
+  filterBtnTextActive: {},
+  filterBtnInactive: {},
+  filterBtnTextInactive: {},
+  sectionHomeComponent: {},
 });
