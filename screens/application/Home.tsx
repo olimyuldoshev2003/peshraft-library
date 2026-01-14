64a2d2921d00d1bc9ecea141992ccc344a98bcb7
Olim Yuldoshev <oyuldoshev39@gmail.com>
2026-01-14 14:08:57 +0500

    Added

--- screens/application/Home.tsx
+++ screens/application/Home.tsx
@@ -42,7 +42,9 @@ const Home = () => {
           <ScrollView
             contentContainerStyle={styles.headerBlock3ScrollView}
             style={styles.headerBlock3}
-            
+            horizontal
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
           >
             <Pressable style={[styles.filterBtn, styles.filterBtnActive]}>
               <Text style={[styles.filterBtnText, styles.filterBtnTextActive]}>
@@ -72,7 +74,45 @@ const Home = () => {
             </Pressable>
           </ScrollView>
         </View>
-        <View style={styles.sectionHomeComponent}></View>
+        <ScrollView
+          contentContainerStyle={styles.sectionHomeComponentScrollView}
+          style={styles.sectionHomeComponent}
+        >
+          <View style={styles.receivedBooks}>
+            <Text style={styles.receivedBookTitle}>Received books</Text>
+            <ScrollView
+              contentContainerStyle={styles.receivedBooksBlockScrollView}
+              style={styles.receivedBooksBlock}
+              horizontal
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}
+            >
+              {/* Received Book 1 */}
+              <View style={styles.receivedBookContainer}>
+                <View style={styles.receivedBookContainerBlock1}>
+                  <Image
+                    source={require("../../assets/peshraft-library/home/tojikon.jpg")}
+                    style={styles.imgReceivedBook}
+                  />
+                </View>
+                <View style={styles.receivedBookContainerBlock2}>
+                  <View style={styles.receivedBookTextBlock}>
+                    <Text style={styles.receivedBookName}></Text>
+                    <Text style={styles.receivedBookAuthor}></Text>
+                  </View>
+                  
+                </View>
+              </View>
+
+              {/* Received Book 2 */}
+              {/* Received Book 3 */}
+            </ScrollView>
+          </View>
+          <View style={styles.allBooks}>
+            <Text style={styles.allBooksTitle}>All books</Text>
+            <View style={styles.allBooksBlock}></View>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
@@ -131,15 +171,55 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     paddingLeft: 55,
   },
-  headerBlock3ScrollView: {},
-  headerBlock3: {
-  
+  headerBlock3ScrollView: {
+    marginTop: 10,
+    gap: 10,
   },
-  filterBtn: {},
-  filterBtnText: {},
-  filterBtnActive: {},
-  filterBtnTextActive: {},
-  filterBtnInactive: {},
-  filterBtnTextInactive: {},
+  headerBlock3: {},
+  filterBtn: {
+    paddingVertical: 3,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+  },
+  filterBtnText: {
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  filterBtnActive: {
+    backgroundColor: "#7EC7EC",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  filterBtnTextActive: {
+    color: "#fff",
+  },
+  filterBtnInactive: {
+    borderWidth: 1,
+    borderColor: "#939393",
+  },
+  filterBtnTextInactive: {
+    color: "#939393",
+  },
   sectionHomeComponent: {},
+  sectionHomeComponentScrollView: {},
+  receivedBooks: {},
+  receivedBookTitle: {},
+  receivedBooksBlockScrollView: {},
+  receivedBooksBlock: {},
+  receivedBookContainer: {},
+  receivedBookContainerBlock1: {},
+  imgReceivedBook: {},
+  receivedBookContainerBlock2: {},
+  receivedBookTextBlock: {},
+  receivedBookName:{},
+  receivedBookAuthor:{},
+  allBooks: {},
+  allBooksTitle: {},
+  allBooksBlock: {},
 });
